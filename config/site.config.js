4312912dbf0d2cf5281cfb3828056dd40ed5776c
Melvin Mmehibe <melvinmmehibe@gmail.com>
2024-03-18 12:20:47 +0100

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -7,7 +7,7 @@ module.exports = {
   // Make sure this is exactly the same as the email address you use to sign into your Microsoft account.
   // You can also put this in your worker's environment variable if you worry about
   // your email being exposed in public.
-  userPrincipalName: process.env.USER_PRINCIPLE_NAME || 'emmaxy@myshutter.app',
+  userPrincipalName: process.env.USER_PRINCIPLE_NAME || 'harry@myshutter.app',
 
   // [OPTIONAL] This is the website icon to the left of the title inside the navigation bar. It should be placed under the
   // /public directory of your GitHub project (not your OneDrive folder!), and referenced here by its relative path to /public.
@@ -39,7 +39,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/MOVIESPLUS/Folder-1'],
+  protectedRoutes: ['/SERIES'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:admin@shutter.ng',
